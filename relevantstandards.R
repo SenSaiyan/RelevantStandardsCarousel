2562ed0b8c9b2c110f661d2b4b873355eae6676a
Daniel Jiang <djiang@ANSI.org>
2019-12-11 17:25:30 -0500

    now properly generates excel file from given standard

--- relevantstandards.R
+++ relevantstandards.R
@@ -3,7 +3,7 @@ library(writexl)
 library(stringr)
 library(dplyr)
 
-xl <<- read_excel("C:/Users/djiang/relevantstandardscarousel/2019 export parsed values.xlsx")
+xl <<- read_excel("C:/Users/djiang/relevantstandardscarousel/2019 export parsed values.xlsx") #change to local location
 standard_input <- trimws("AAMI TIR12:2010 (AAMI TIR 12:2010)") #input valid product id
 customers <- filter(xl, ProductId == standard_input)
 total_orders <- data.frame(OrderNumber = character())
@@ -15,10 +15,10 @@ for(orders in 1:nrow(customers)){
   other_ordered_standards <- rbind(other_ordered_standards, temp)
 }
 
-other_standards_bought <- filter(other_ordered_standards, ProductId != standard_input) #filter out own standard name
+other_standards_bought <- filter(other_ordered_standards, ProductId != standard_input)
 occurances <- table(unlist(other_standards_bought$ProductId))
 occurances_df <- as.data.frame(occurances)
 occurances_ordered <- arrange(occurances_df, desc(Freq))
 filename_clean <- str_remove_all(standard_input, "[(): ]")
-filepath <- paste("C:/Users/djiang/RelevantStandardsCarousel/", filename_clean, '.xlsx', sep = "")
+filepath <- paste("C:/Users/djiang/Documents/", filename_clean, '.xlsx', sep = "") #change to local folder
 tmp <- write_xlsx(occurances_ordered, filepath)
